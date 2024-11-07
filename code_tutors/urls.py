b4bb5b42d91658ea59a5eb25f6ee7ab9c243d7f0
arhamzahid23571113 <146372787+arhamzahid23571113@users.noreply.github.com>
2024-11-07 15:18:42 +0000

    Add routes for role-based dashboards and improve URL structure

--- code_tutors/urls.py
+++ code_tutors/urls.py
@@ -23,11 +23,17 @@ from tutorials import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    
     path('log_in/', views.LogInView.as_view(), name='log_in'),
     path('log_out/', views.log_out, name='log_out'),
     path('password/', views.PasswordView.as_view(), name='password'),
     path('profile/', views.ProfileUpdateView.as_view(), name='profile'),
     path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
+    
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('tutor_dashboard/', views.tutor_dashboard, name='tutor_dashboard'),
+    path('student_dashboard/', views.student_dashboard, name='student_dashboard'),
 ]
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
