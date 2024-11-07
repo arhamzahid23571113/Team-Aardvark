b8ed7ce114cecd55e5eb86c1279f978ed4875932
arhamzahid23571113 <146372787+arhamzahid23571113@users.noreply.github.com>
2024-11-07 16:31:54 +0000

    Improve User model tests for validation and custom methods

--- tutorials/tests/models/test_user_model.py
+++ tutorials/tests/models/test_user_model.py
@@ -19,6 +19,7 @@ class UserModelTestCase(TestCase):
     def test_valid_user(self):
         self._assert_user_is_valid()
 
+    # Username Tests
     def test_username_cannot_be_blank(self):
         self.user.username = ''
         self._assert_user_is_invalid()
@@ -36,35 +37,10 @@ class UserModelTestCase(TestCase):
         self.user.username = second_user.username
         self._assert_user_is_invalid()
 
-    def test_username_must_start_with_at_symbol(self):
-        self.user.username = 'johndoe'
-        self._assert_user_is_invalid()
-
-    def test_username_must_contain_only_alphanumericals_after_at(self):
-        self.user.username = '@john!doe'
-        self._assert_user_is_invalid()
-
-    def test_username_must_contain_at_least_3_alphanumericals_after_at(self):
-        self.user.username = '@jo'
-        self._assert_user_is_invalid()
-
-    def test_username_may_contain_numbers(self):
-        self.user.username = '@j0hndoe2'
-        self._assert_user_is_valid()
-
-    def test_username_must_contain_only_one_at(self):
-        self.user.username = '@@johndoe'
-        self._assert_user_is_invalid()
-
-
+    # First Name Tests
     def test_first_name_must_not_be_blank(self):
         self.user.first_name = ''
         self._assert_user_is_invalid()
-
-    def test_first_name_need_not_be_unique(self):
-        second_user = User.objects.get(username='@janedoe')
-        self.user.first_name = second_user.first_name
-        self._assert_user_is_valid()
 
     def test_first_name_may_contain_50_characters(self):
         self.user.first_name = 'x' * 50
@@ -74,15 +50,10 @@ class UserModelTestCase(TestCase):
         self.user.first_name = 'x' * 51
         self._assert_user_is_invalid()
 
-
+    # Last Name Tests
     def test_last_name_must_not_be_blank(self):
         self.user.last_name = ''
         self._assert_user_is_invalid()
-
-    def test_last_name_need_not_be_unique(self):
-        second_user = User.objects.get(username='@janedoe')
-        self.user.last_name = second_user.last_name
-        self._assert_user_is_valid()
 
     def test_last_name_may_contain_50_characters(self):
         self.user.last_name = 'x' * 50
@@ -92,7 +63,7 @@ class UserModelTestCase(TestCase):
         self.user.last_name = 'x' * 51
         self._assert_user_is_invalid()
 
-
+    # Email Tests
     def test_email_must_not_be_blank(self):
         self.user.email = ''
         self._assert_user_is_invalid()
@@ -102,56 +73,59 @@ class UserModelTestCase(TestCase):
         self.user.email = second_user.email
         self._assert_user_is_invalid()
 
-    def test_email_must_contain_username(self):
-        self.user.email = '@example.org'
-        self._assert_user_is_invalid()
-
     def test_email_must_contain_at_symbol(self):
         self.user.email = 'johndoe.example.org'
         self._assert_user_is_invalid()
 
-    def test_email_must_contain_domain_name(self):
-        self.user.email = 'johndoe@.org'
+    # Role Field Tests
+    def test_valid_roles(self):
+        for role in ['admin', 'tutor', 'student']:
+            self.user.role = role
+            self._assert_user_is_valid()
+
+    def test_invalid_role(self):
+        self.user.role = 'invalid_role'
         self._assert_user_is_invalid()
 
-    def test_email_must_contain_domain(self):
-        self.user.email = 'johndoe@example'
-        self._assert_user_is_invalid()
+    # Expertise Field Tests
+    def test_expertise_can_be_blank(self):
+        self.user.expertise = ''
+        self._assert_user_is_valid()
 
-    def test_email_must_not_contain_more_than_one_at(self):
-        self.user.email = 'johndoe@@example.org'
-        self._assert_user_is_invalid()
+    def test_expertise_can_contain_topics(self):
+        self.user.expertise = 'Python, Django'
+        self._assert_user_is_valid()
 
+    # Lesson Preferences Field Tests
+    def test_lesson_preferences_can_be_blank(self):
+        self.user.lesson_preferences = ''
+        self._assert_user_is_valid()
 
+    def test_lesson_preferences_can_contain_preferences(self):
+        self.user.lesson_preferences = 'Python Basics'
+        self._assert_user_is_valid()
+
+    # Custom Methods Tests
     def test_full_name_must_be_correct(self):
-        full_name = self.user.full_name()
-        self.assertEqual(full_name, "John Doe")
-
+        self.assertEqual(self.user.full_name(), "John Doe")
 
     def test_default_gravatar(self):
-        actual_gravatar_url = self.user.gravatar()
-        expected_gravatar_url = self._gravatar_url(size=120)
-        self.assertEqual(actual_gravatar_url, expected_gravatar_url)
+        self.assertEqual(self.user.gravatar(), self._gravatar_url(size=120))
 
     def test_custom_gravatar(self):
-        actual_gravatar_url = self.user.gravatar(size=100)
-        expected_gravatar_url = self._gravatar_url(size=100)
-        self.assertEqual(actual_gravatar_url, expected_gravatar_url)
+        self.assertEqual(self.user.gravatar(size=100), self._gravatar_url(size=100))
 
     def test_mini_gravatar(self):
-        actual_gravatar_url = self.user.mini_gravatar()
-        expected_gravatar_url = self._gravatar_url(size=60)
-        self.assertEqual(actual_gravatar_url, expected_gravatar_url)
+        self.assertEqual(self.user.mini_gravatar(), self._gravatar_url(size=60))
 
+    # Helper Methods
     def _gravatar_url(self, size):
-        gravatar_url = f"{UserModelTestCase.GRAVATAR_URL}?size={size}&default=mp"
-        return gravatar_url
-
+        return f"{UserModelTestCase.GRAVATAR_URL}?size={size}&default=mp"
 
     def _assert_user_is_valid(self):
         try:
             self.user.full_clean()
-        except (ValidationError):
+        except ValidationError:
             self.fail('Test user should be valid')
 
     def _assert_user_is_invalid(self):
