a4e35d46810682e6d8b5cbda0d8220cb40af0bc8
Abiha-t <abiha.tasnim@gmail.com>
2024-11-24 18:12:02 +0000

    change icon for my_tutor_profile

--- tutorials/views.py
+++ tutorials/views.py
@@ -163,4 +163,4 @@ def contact_admin(request):
     return render(request,'contact_admin.html')
 
 def see_my_tutor_profile(request):
-    return render(request,'my_tutor_profile')
+    return render(request,'my_tutor_profile.html')
