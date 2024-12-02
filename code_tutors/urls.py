1b984042d96926f53bfedbb6a47aca9bfcf05bc6
Abiha-t <abiha.tasnim@gmail.com>
2024-12-02 14:16:36 +0000

    create url for tutor_more_info

--- code_tutors/urls.py
+++ code_tutors/urls.py
@@ -56,6 +56,8 @@ urlpatterns = [
     path('tutor-profile/<int:tutor_id>/', views.view_tutor_profile, name='view_tutor_profile'),
     path('tutor-profile/<int:tutor_id>/edit/', views.edit_tutor_profile, name='edit_tutor_profile'),
     path('all-students/', views.all_student_profiles, name='all_students'),
+    path('tutor/<int:tutor_id>/info/', views.tutor_more_info, name='tutor_more_info'),
+
 
 
 
