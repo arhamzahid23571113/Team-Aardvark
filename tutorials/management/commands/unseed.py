b0a9def598ed871ce4a7adf7186688e07b14e734
arhamzahid23571113 <146372787+arhamzahid23571113@users.noreply.github.com>
2024-12-03 00:47:55 +0000

    Enhance unseeder to remove all seeded data from the database

--- tutorials/management/commands/unseed.py
+++ tutorials/management/commands/unseed.py
@@ -1,12 +1,27 @@
-from django.core.management.base import BaseCommand, CommandError
-from tutorials.models import User
+from django.core.management.base import BaseCommand
+from tutorials.models import User, LessonRequest, LessonBooking, Invoice
 
 class Command(BaseCommand):
     """Build automation command to unseed the database."""
-    
-    help = 'Seeds the database with sample data'
+
+    help = 'Unseeds the database by removing all seeded data'
 
     def handle(self, *args, **options):
         """Unseed the database."""
+        # Remove invoices
+        Invoice.objects.all().delete()
+        self.stdout.write("All invoices removed.")
 
+        # Remove lesson bookings
+        LessonBooking.objects.all().delete()
+        self.stdout.write("All lesson bookings removed.")
+
+        # Remove lesson requests
+        LessonRequest.objects.all().delete()
+        self.stdout.write("All lesson requests removed.")
+
+        # Remove non-staff users
         User.objects.filter(is_staff=False).delete()
+        self.stdout.write("All non-staff users removed.")
+
+        self.stdout.write("Database unseeding complete.")
