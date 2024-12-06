b9e3b7e56bde62a465fcedd06174c70a57f52b17
Abiha-t <abiha.tasnim@gmail.com>
2024-12-06 23:40:37 +0000

    ensure that messages can be viewed by admin from students

--- tutorials/models.py
+++ tutorials/models.py
@@ -158,7 +158,7 @@ class ContactMessage(models.Model):
         ('student', 'Student'),
         ('tutor', 'Tutor'),
     ]
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages',default=1)
     role = models.CharField(max_length=10, choices = ROLES)
     message = models.TextField(
         blank = True,
