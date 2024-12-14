b3528513e098dddbff1f9d630fd2df11372879fc
Abiha-t <abiha.tasnim@gmail.com>
2024-12-14 23:47:19 +0000

    update admin reply test cases

--- tutorials/tests/forms/test_AdminReplyBack_form.py
+++ tutorials/tests/forms/test_AdminReplyBack_form.py
@@ -11,10 +11,6 @@ class AdminReplyBackFormTest(TestCase):
         form = AdminReplyBack(data=self.valid_data)
         self.assertTrue(form.is_valid())
 
-    def test_form_is_valid_with_empty_reply(self):
-        form = AdminReplyBack(data=self.empty_data)
-        self.assertTrue(form.is_valid())
-
     def test_form_has_correct_placeholder_in_widget(self):
         form = AdminReplyBack()
         self.assertIn('placeholder', form.fields['reply'].widget.attrs)
