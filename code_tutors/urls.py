94b501309bcb9ac8b2b6e4023def0929c45f427f
arhamzahid23571113 <146372787+arhamzahid23571113@users.noreply.github.com>
2024-12-17 17:51:30 +0000

    Update urls.py

--- code_tutors/urls.py
+++ code_tutors/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('log_out/', views.log_out, name='log_out'),
     path('password/', views.PasswordView.as_view(), name='password'),
     path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
+    path('profile/', views.ProfileUpdateView.as_view(), name='profile'),
     
     # Password Reset URLs
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
