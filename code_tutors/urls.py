c0ccbe991bdb717454e933a6ebadc9ac3983c633
arhamzahid23571113 <146372787+arhamzahid23571113@users.noreply.github.com>
2024-12-17 17:36:35 +0000

    Remove redundant url path

--- code_tutors/urls.py
+++ code_tutors/urls.py
@@ -32,7 +32,6 @@ urlpatterns = [
     path('log_in/', views.LogInView.as_view(), name='log_in'),
     path('log_out/', views.log_out, name='log_out'),
     path('password/', views.PasswordView.as_view(), name='password'),
-    path('profile/', views.edit_profile, name='profile'),
     path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
     
     # Password Reset URLs
